f89d2ba9117274210cfa86dcc9e3f97e365957e5
veronia ashraf <126025103+veronnia@users.noreply.github.com>
2025-07-17 17:11:32 +0300

    almost finished

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,4 @@
+const sketch = document.querySelector('.grd');
 let slider = document.getElementById("myRange");
 let size = 25;
 let output = document.getElementById("value");
@@ -6,5 +7,34 @@ let progress = document.getElementById("prog");
 slider.oninput = function() {
     output.innerHTML = this.value;
     progress.value = slider.value;  
+    deleteGrid();
+    createGrid(this.value);
+        
 }
 
+createGrid(size);
+
+let squares = document.querySelectorAll('.squares');
+
+function createGrid(size) {
+    let area = 500 / size;
+
+    for(let i=0; i<size; i++) {
+        for(let j=0; j<size; j++) {
+            let square = document.createElement("div");
+            square.classList.add('squares');
+            square.style.width = `${area}px`;
+            square.style.height = `${area}px`;
+            console.log("hi");
+            
+            sketch.append(square);
+        }
+    }
+}
+
+function deleteGrid() {
+    while (sketch.firstChild) {
+        sketch.lastChild = null;
+        sketch.removeChild(sketch.lastChild);
+    }
+}
